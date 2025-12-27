2c9faf7f2d51cd7a1ac190876cefffdc0cc38478
m1chael888 <sofishmain@gmail.com>
2025-12-27 14:01:32 +0200

    +allow users to conduct new calculations using results from calculation history

--- Calculator.m1chael888/Program.cs
+++ Calculator.m1chael888/Program.cs
@@ -8,11 +8,12 @@ namespace CalculatorProgram.m1chael888
         static void Main(string[] args)
         {
             var calc = new Calculator();
-            var history = new Dictionary<int, string>();
+            var history = new Dictionary<List<double>, string>();
             int count = 0;
 
             Menu();
 
+            //////////
             void Menu()
             {
                 Console.Clear();
@@ -22,12 +23,11 @@ namespace CalculatorProgram.m1chael888
                 Console.WriteLine("2 - View calculation history");
                 Console.WriteLine("3 - Exit program");
                 Console.Write("\nEnter the number of your desired menu option: ");
+                string input = Console.ReadLine();
 
                 bool done = false;
                 while (!done)
                 {
-                    string input = Console.ReadLine();
-
                     switch (input)
                     {
                         case "1":
@@ -43,11 +43,13 @@ namespace CalculatorProgram.m1chael888
                             break;
                         default:
                             Console.Write("Invalid choice. Please enter a valid menu option (1-3): ");
+                            input = Console.ReadLine();
                             break;
                     }
                 }
             }
 
+            /////////////////
             void ViewHistory()
             {
                 Console.Clear();
@@ -69,67 +71,119 @@ namespace CalculatorProgram.m1chael888
                 }
 
                 Console.WriteLine("------------------------");
-                Console.WriteLine("\n1 - Delete History (This cannot be undone)");
-                Console.WriteLine("2 - Return to menu");
-                Console.WriteLine("3 - Exit program");
+                Console.WriteLine("- What's Next? -");
+                Console.WriteLine("------------------------\n");
+                Console.WriteLine("1 - New calculation with previous results");
+                Console.WriteLine("2 - Delete History (This cannot be undone)");
+                Console.WriteLine("3 - Return to menu");
+                Console.WriteLine("4 - Exit program");
                 Console.Write("\nHow would you like to proceed? Choose an option: ");
+                string input = Console.ReadLine();
 
                 bool done = false;
                 while (!done)
                 {
-                    string input = Console.ReadLine();
                     switch (input)
                     {
                         case "1":
+                            ResultCalculation();
+                            break;
+                        case "2":
                             done = true;
                             history.Clear();
                             ViewHistory();
                             break;
-                        case "2":
+                        case "3":
                             done = true;
                             Menu();
                             break;
-                        case "3":
+                        case "4":
                             Environment.Exit(0);
                             break;
                         default:
-                            Console.Write("Invalid choice. Please enter a valid menu option (1-3): ");
+                            Console.Write("Invalid choice. Please enter a valid menu option (1-4): ");
+                            input = Console.ReadLine();
                             break;
                     }
                 }
             }
 
-            void Calculation()
+            ///////////////////////
+            void ResultCalculation()
             {
-                Console.Clear();
+                string input1 = ""; double num1;
+                string input2 = ""; double num2;
+
+                if (history.Count() > 1)
+                {
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("- Conduct calculation with existing results -");
+                    Console.WriteLine("------------------------\n");
+
+                    Console.Write("Enter the Question number representing the first result youd like to use: ");
+                    input1 = Console.ReadLine();
+                    while (!(Convert.ToInt32(input1) >= 0 && Convert.ToInt32(input1) <= history.Count))
+                    {
+                        Console.Write($"This is not valid input. Please enter a numeric value (1-{history.Count}): ");
+                        input1 = Console.ReadLine();
+                    }
+                    num1 = GetResult(Convert.ToDouble(input1));
+                    Console.WriteLine($"First number: {num1}");
+
+                    Console.Write("Enter the Question number representing the second result youd like to use: ");
+                    input2 = Console.ReadLine();
+                    while (!(Convert.ToInt32(input2) >= 0 && Convert.ToInt32(input2) <= history.Count))
+                    {
+                        Console.Write($"This is not valid input. Please enter a numeric value (1-{history.Count}): ");
+                        input2 = Console.ReadLine();
+                    }
+                    num2 = GetResult(Convert.ToDouble(input2));
+                    Console.WriteLine($"Second number: {num2}");
+
+                    Calculation(num1.ToString(), num2.ToString());
+                }
+                else
+                {
+                    Console.Write("There are not enough existing results to calculate with. Choose a different option: ");
+                }
+            }
+
+            /////////////////
+            void Calculation(string input1 = "", string input2 = "")
+            {
                 bool done = false;
                 while (!done)
                 {
-                    string? input1 = "";
-                    string? input2 = "";
                     double result = 0;
 
-                    Console.WriteLine("- Conduct Calculation -");
-                    Console.WriteLine("------------------------\n");
-                    Console.Write("Type a number, and then press Enter: ");
-                    input1 = Console.ReadLine();
-
-                    double cleanNum1 = 0;
-                    while (!double.TryParse(input1, out cleanNum1))
+                    if (input1 == "" && input2 == "")
                     {
-                        Console.Write("This is not valid input. Please enter a numeric value: ");
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine("- Conduct Calculation -");
+                        Console.WriteLine("------------------------\n");
+                        Console.Write("Type a number, and then press Enter: ");
                         input1 = Console.ReadLine();
-                    }
 
-                    Console.Write("Type another number, and then press Enter: ");
-                    input2 = Console.ReadLine();
+                        double num1 = 0;
+                        while (!double.TryParse(input1, out num1))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            input1 = Console.ReadLine();
+                        }
 
-                    double cleanNum2 = 0;
-                    while (!double.TryParse(input2, out cleanNum2))
-                    {
-                        Console.Write("This is not valid input. Please enter a numeric value: ");
+                        Console.Write("Type another number, and then press Enter: ");
                         input2 = Console.ReadLine();
+
+                        double num2 = 0;
+                        while (!double.TryParse(input2, out num2))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            input2 = Console.ReadLine();
+                        }
                     }
+                    
+                    double cleanNum1 = Convert.ToDouble(input1);
+                    double cleanNum2 = Convert.ToDouble(input2);
 
                     bool done2 = false;
                     while (!done2)
@@ -140,54 +194,40 @@ namespace CalculatorProgram.m1chael888
                         Console.WriteLine("\tm - Multiply");
                         Console.WriteLine("\td - Divide");
                         Console.Write("\nYour choice: ");
-                        string? op = Console.ReadLine();
+                        string op = Console.ReadLine();
 
-                        if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+                        while (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
                         {
-                            Console.WriteLine("Invalid choice. Please enter a valid operand");
+                            Console.Write("Invalid choice. Please enter a valid operand: ");
+                            op = Console.ReadLine();
                         }
 
-                        else
+                        try
                         {
-                            try
+                            result = calc.DoOperation(cleanNum1, cleanNum2, op);
+                            if (double.IsNaN(result))
                             {
-                                result = calc.DoOperation(cleanNum1, cleanNum2, op);
-                                if (double.IsNaN(result))
-                                {
-                                    Console.WriteLine("This operation will result in a math error\n");
-                                }
-                                else
-                                {
-                                    Console.Clear();
-                                    Console.WriteLine("- Whats next? -");
-                                    Console.WriteLine("------------------------\n");
-                                    Console.WriteLine("Your result: {0:0.##}", result);
-
-                                    switch (op)
-                                    {
-                                        case "a":
-                                            op = "+";
-                                            break;
-                                        case "s":
-                                            op = "-";
-                                            break;
-                                        case "m":
-                                            op = "x";
-                                            break;
-                                        case "d":
-                                            op = "/";
-                                            break;
-                                    }
-                                    count++;
-                                    history.Add(count, $"{cleanNum1} {op} {cleanNum2} = {result}");
-                                    Console.WriteLine($"\nTotal calculations completed: {count}");
-                                }
+                                Console.WriteLine("This operation will result in a math error\n");
                             }
-                            catch (Exception ex)
+                            else
                             {
-                                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + ex.Message);
+                                Console.Clear();
+                                Console.WriteLine("- Calcuation complete -");
+                                Console.WriteLine("------------------------\n");
+                                Console.WriteLine("Your result: {0:0.##}", result);
+
+                                count++;
+                                history.Add([count, result], $"{cleanNum1} {GetOp(op)} {cleanNum2} = {result}");
+                                Console.WriteLine($"\nTotal calculations completed: {count}");
                             }
                         }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + ex.Message);
+                        }
+                        
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine("- What's Next? -");
                         Console.WriteLine("------------------------\n");
 
                         bool done3 = false;
@@ -203,6 +243,7 @@ namespace CalculatorProgram.m1chael888
                             {
                                 case "1":
                                     done2 = true;
+                                    Console.Clear();
                                     Calculation();
                                     break;
                                 case "2":
@@ -213,7 +254,8 @@ namespace CalculatorProgram.m1chael888
                                     Environment.Exit(0);
                                     break;
                                 default:
-                                    Console.WriteLine("Invalid choice. Please enter a valid menu option (1-3");
+                                    Console.Write("Invalid choice. Please enter a valid menu option (1-3): ");
+                                    input = Console.ReadLine();
                                     break;
                             }
                         }
@@ -221,6 +263,38 @@ namespace CalculatorProgram.m1chael888
                     calc.Finish();
                     return;
                 }
+            }
+
+            /////////////
+            string GetOp(string op)
+            {
+                switch (op)
+                {
+                    case "a":
+                        op = "+";
+                        break;
+                    case "s":
+                        op = "-";
+                        break;
+                    case "m":
+                        op = "x";
+                        break;
+                    case "d":
+                        op = "/";
+                        break;
+                }
+
+                return op;
+            }
+
+            /////////////////
+            double GetResult(double id)
+            {
+                foreach (var record in history)
+                {
+                    if (record.Key[0] == id) return record.Key[1];
+                }
+                return 0;
             }
         }
     }
